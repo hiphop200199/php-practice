34d3718f138424e318153d53907012c8290396dc
Eric Tsai <hiphop200199@gmail.com>
2022-12-24 16:37:02 +0800

    Delete crud directory

--- crud/read.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-include("db-connection.php") ;
-
-$sql = "SELECT * FROM course ORDER BY course_id DESC";
-$statement = $conn->prepare($sql);
-$statement->execute();
-
-$result=$statement->setFetchMode(PDO::FETCH_ASSOC);
